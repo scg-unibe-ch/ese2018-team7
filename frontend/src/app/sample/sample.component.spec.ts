f8fb1e749d3230d4044cac26cc628851a1c7a8bb
Noah Bühlmann <noah.buehlmann@students.unibe.ch>
2018-09-25 22:51:50 +0200

    initial commit

---
+++ frontend/src/app/sample/sample.component.spec.ts
@@ -0,0 +1,28 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {AppModule} from '../app.module';
+import {SampleComponent} from './sample.component';
+
+describe('SampleComponent', () => {
+  let component: SampleComponent;
+  let fixture: ComponentFixture<SampleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AppModule
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SampleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/weather.service.spec.ts
@@ -0,0 +1,18 @@
+import {TestBed, inject} from '@angular/core/testing';
+
+import {WeatherService} from './weather.service';
+import {AppModule} from './app.module';
+
+describe('WeatherService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AppModule
+      ]
+    });
+  });
+
+  it('should be created', inject([WeatherService], (service: WeatherService) => {
+    expect(service).toBeTruthy();
+  }));
+});
