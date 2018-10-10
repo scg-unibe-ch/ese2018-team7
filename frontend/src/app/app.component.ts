f306016dd26cdbe480bac56f4b6dee55948a2c7a
Marcel Würsten <marcel.wuersten@students.unibe.ch>
2018-10-10 21:42:41 +0200

    Redesigned the Menusystem

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
 import {AuthService} from './auth/auth.service';
 
 @Component({
@@ -18,62 +16,10 @@ export class AppComponent implements OnInit {
     {link: '/logout', text: 'Logout', condition: function() {return AuthService.isLogin(); }},
     ];
 
-// <a routerLink="/changePassword" routerLinkActive="{{changePassword}}">Change Password</a>
-  loggedin = false;
-
-  logInOutLink = '/login';
-  logInOutText = 'Login';
-
-  jobViewEditLink = '/viewJobs';
-  jobViewEditText = 'View Jobs';
-
-  changePassword = 'disabled';
-
-  constructor(private httpClient: HttpClient, router: Router) {
-    this.onUpdate({'id': ''});
-    router.events.subscribe((val) => {
-      // see also
-      this.onUpdate(val);
-    });
-    // r
+  constructor() {
   }
 
   ngOnInit() {
-  }
-  onUpdate(params:  any) {
-    console.log('update menu ' + params['id']);
-
-    this.httpClient.get('http://localhost:3000/login/check', {withCredentials: true}).subscribe(
-      (res: any) => {
-        if (res.value === null || res.value !== 'true') {
-          if (this.loggedin === true) {
-            this.logInOutLink = '/login';
-            this.logInOutText = 'Login';
-
-            this.jobViewEditLink = '/viewJobs';
-            this.jobViewEditText = 'View Jobs';
-
-            this.changePassword = 'disabled';
-          }
-          this.loggedin = false;
-
-        } else {
-          if (this.loggedin === false) {
-            this.logInOutLink = '/logout';
-            this.logInOutText = 'Logout';
-
-            this.jobViewEditLink = '/editJobs';
-            this.jobViewEditText = 'Edit Jobs';
-
-            this.changePassword = 'active';
-          }
-
-          this.loggedin = true;
-
-        }
-      }
-    );
-
   }
 
 }
