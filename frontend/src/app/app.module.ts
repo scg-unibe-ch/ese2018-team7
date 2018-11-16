b60693b13bc37cbb20754e648e35a211d360bdef
Marcel Würsten <marcel.wuersten@students.unibe.ch>
2018-11-16 17:54:01 +0100

    Added Markdown

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -36,6 +36,8 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { CdkTableModule} from '@angular/cdk/table';
 
 import {AutosizeModule} from 'ngx-autosize';
+import {MarkdownModule} from 'ngx-markdown';
+import { AngularMarkdownEditorModule } from 'angular-markdown-editor';
 import {MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
 import { Ng5SliderModule } from 'ng5-slider';
 import {MatDatepickerModule, MatMomentDateModule, MAT_DATE_LOCALE, MAT_DATE_FORMATS} from '@coachcare/datepicker';
@@ -120,6 +122,8 @@ import {JobsSortPipe} from './jobsView/jobsSort.pipe';
     CdkTableModule,
     MatSnackBarModule,
     MatSelectModule,
+    MarkdownModule.forRoot(),
+    AngularMarkdownEditorModule.forRoot(),
   ],
   entryComponents: [
     JobViewDetailsComponent,
